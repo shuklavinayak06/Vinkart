d9c35af9ad17fe5732e56f5d828d1d919101f48e
shuklavinayak06 <vinayak.shukla06@gmail.com>
2026-05-21 15:41:21 +0530

    fixed vite env config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://vinkart-server.onrender.com')
+  }
 })
